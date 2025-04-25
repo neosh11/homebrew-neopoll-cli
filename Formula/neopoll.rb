c7e5ddd76e47ee7f0c11fe82c8ed1cd47e0219ad
Neosh <neosh11@gmail.com>
2025-04-25 15:36:57 +1000

    bump

--- Formula/neopoll.rb
+++ Formula/neopoll.rb
@@ -1,8 +1,8 @@
 class Neopoll < Formula
     desc     "CLI poll application"
     homepage "https://github.com/neosh11/homebrew-neopoll-cli"
-    url      "https://github.com/neosh11/homebrew-neopoll-cli/archive/refs/tags/v0.1.2.tar.gz"
-    sha256   "414709c47a3e7671a97cb1f5827d4785b580a95e80fe8787d3b93d06c5c9953d"
+    url      "https://github.com/neosh11/homebrew-neopoll-cli/archive/refs/tags/v0.1.3.tar.gz"
+    sha256   "6de30b76159acd176af667fa63725429c8162fc8097fc677a512da2c67c778dc"
     license  "MIT"
     depends_on "go" => :build
   
